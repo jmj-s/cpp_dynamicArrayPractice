3b30e014a3a9c66bf45cff42e8de36e37761acb2
sweiss8 <jmj.stuartweiss@gmail.com>
2021-04-30 13:40:04 -0700

    Initial Commit

---
+++ cpp_dynamicArrayPractice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_dynamicArrayPractice
+//
+//  Created by Stuart Weiss on 4/30/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
